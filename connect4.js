02d36c83e3de9688a2245af1673335cb4e5c860f
Gerardo Huerta <gerardoh13@gmail.com>
2022-08-11 16:57:59 -0700

    styling changes, comments added, and minor JS tweaks

--- connect4.js
+++ connect4.js
@@ -4,17 +4,13 @@
  * column until a player gets four-in-a-row (horiz, vert, or diag) or until
  * board fills (tie)
  */
-
 const WIDTH = 7;
 const HEIGHT = 6;
-
 let currPlayer = 1; // active player: 1 or 2
 let board = []; // array of rows, each row is array of cells  (board[y][x])
-
 /** makeBoard: create in-JS board structure:
  *    board = array of rows, each row is array of cells  (board[y][x])
  */
-
 function makeBoard() {
   // TODO: set "board" to empty HEIGHT x WIDTH matrix array
   //aded this to reset the board onclick
@@ -25,9 +21,7 @@ function makeBoard() {
     board.push(Array(WIDTH).fill(null));
   }
 }
-
 /** makeHtmlBoard: make HTML table and row of column tops. */
-
 function makeHtmlBoard() {
   // TODO: get "htmlBoard" variable from the item in HTML w/ID of "board"
   const htmlBoard = document.getElementById("board");
@@ -41,20 +35,23 @@ function makeHtmlBoard() {
   // added event listeners for mouse hovering over the top row
   top.addEventListener("mouseover", onMouseOver);
   top.addEventListener("mouseout", onMouseOut);
-
+  // creates td elements for the top row, sets the id to the index, and appends to the top row
   for (let x = 0; x < WIDTH; x++) {
     const headCell = document.createElement("td");
     headCell.setAttribute("id", x);
     top.append(headCell);
   }
   htmlBoard.append(top);
-
   // TODO: add comment for this code
+  // a nested for loop that creates rows in the top loop, and tds in the inner loop to fill the rows.
   for (let y = 0; y < HEIGHT; y++) {
     const row = document.createElement("tr");
+    //added classes to the each row to be able to add a pseudo gradient look to my board
+    row.classList.add(`row${y}`)
     for (let x = 0; x < WIDTH; x++) {
       const cell = document.createElement("td");
       cell.setAttribute("id", `${y}-${x}`);
+      // added an aditional div to help with styling the board to look like the classic game
         let roundDiv = document.createElement('div')
         roundDiv.classList.add('roundDiv')
         cell.append(roundDiv)
@@ -73,9 +70,7 @@ function onMouseOver(e) {
 function onMouseOut(e) {
   e.target.firstChild.remove();
 }
-
 /** findSpotForCol: given column x, return top empty y (null if filled) */
-
 function findSpotForCol(x) {
   // TODO: write the real version of this, rather than always returning 0
   for (let i = board.length - 1; i > -1; i--) {
@@ -85,9 +80,7 @@ function findSpotForCol(x) {
   }
   return null;
 }
-
 /** placeInTable: update DOM to place piece into HTML table of board */
-
 function placeInTable(y, x) {
   // TODO: make a div and insert into correct table cell
   let td = document.getElementById(`${y}-${x}`);
@@ -95,9 +88,7 @@ function placeInTable(y, x) {
   newDiv.classList.add("piece", `player${currPlayer}`, "drop");
   td.append(newDiv);
 }
-
 /** endGame: announce game end */
-
 function endGame(msg) {
   // remove the event listener so user can't click on top row if game is over.
   let top = document.getElementById("column-top");
@@ -105,11 +96,9 @@ function endGame(msg) {
   // TODO: pop up alert message
   setTimeout(() => {
     alert(msg);
-  }, 500);
+  }, 600);
 }
-
 /** handleClick: handle click of column top to play piece */
-
 function handleClick(evt) {
   // get x from ID of clicked cell
   const x = +evt.target.id;
@@ -123,12 +112,10 @@ function handleClick(evt) {
   // board[]
   board[y][x] = currPlayer;
   placeInTable(y, x);
-
   // check for win
   if (checkForWin()) {
     return endGame(`Player ${currPlayer} won!`);
   }
-
   // check for tie
   // TODO: check if all cells in board are filled; if so call, call endGame
   // only top row needs to be checked, if all values in top row !== null, passes tie message to endGame func
@@ -137,7 +124,6 @@ function handleClick(evt) {
   }
   // switch players
   // TODO: switch currPlayer 1 <-> 2
-
   currPlayer === 1 ? (currPlayer = 2) : (currPlayer = 1);
   // removes the previous player's piece and appends a new one for the next player
   evt.target.firstChild.remove();
@@ -145,9 +131,7 @@ function handleClick(evt) {
   newPiece.classList.add("piece", `player${currPlayer}`);
   evt.target.append(newPiece);
 }
-
 /** checkForWin: check board cell-by-cell for "does a win start here?" */
-
 function checkForWin() {
   function _win(cells) {
     // Check four cells to see if they're all color of current player
@@ -168,9 +152,8 @@ function checkForWin() {
       return true;
     }
   }
-
   // TODO: read and understand this code. Add comments to help you.
-
+  // nested for loops that iterates through all possible horizontal, vertical and diagonal 4 cell possibilities.
   for (let y = 0; y < HEIGHT; y++) {
     for (let x = 0; x < WIDTH; x++) {
       let horiz = [
@@ -197,7 +180,7 @@ function checkForWin() {
         [y + 2, x - 2],
         [y + 3, x - 3],
       ];
-
+      // if any of the possible matches returns true when checked by _win func, this nested loop will return true.
       if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) {
         return true;
       }
@@ -206,16 +189,19 @@ function checkForWin() {
 }
 //adds spin class to pieces in winning cells to display spinning animation
 function winAnimation(cells) {
+  setTimeout(() => {
   for (let cell of cells) {
     document
       .getElementById(`${cell[0]}-${cell[1]}`).firstChild.nextElementSibling.classList.add("spin");
   }
+}, 550)
 }
-//aded this to reset the board onclick
+//aded this func to reset the board onclick.
 function reset() {
   makeBoard();
   makeHtmlBoard();
 }
+// event listener for reset button. removes spin class and adds reset class for falling pieces animation. it then calls reset function
 let resetBtn = document.getElementById("reset")
 resetBtn.addEventListener('click', () => {
   let pieces = document.querySelectorAll(".piece")
